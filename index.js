6cc946ebcdb9c08663dd1a1f2e7a7860ff45cec2
Craig Miller <craigmiller160@gmail.com>
2019-11-30 17:15:34 -0500

    Began building out the app

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const fs = require('fs');
+const path = require('path');
+
+const DATA_PATH = path.resolve(__dirname, './data/list.json');
+
+const readData = () => {
+	if (fs.existsSync(DATA_PATH)) {
+		const dataJson = fs.readFileSync(DATA_PATH, 'utf8');
+		return JSON.parse(dataJson);
+	}
+	return [];
+};
+
+const data = readData();
 
