807190f0fabd99e132790985914179e6455dbbcb
Craig Miller <craigmiller160@gmail.com>
2019-12-01 09:29:43 -0500

    Finished initial menu

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const commander = require('commander');
-const { readData, writeData } = require('./scripts/io');
 
-const data = readData();
+commander
+	.option('-a, --add', 'Add an item to the shopping list')
+	.option('-r, --remove', 'Remove an item to the shopping list')
+	.option('-p, --print', 'Print the shopping list')
+	.parse(process.argv);
 
-console.log(data);
+if (commander.add) {
+	throw new Error('Adding not implemented yet');
+}
+else if (commander.remove) {
+	throw new Error('Removing not implemented yet');
+} 
+else if (commander.print) {
+	throw new Error('Printing not implemented yet');
+}
+else {
+	throw new Error('Invalid arguments.');
+}
