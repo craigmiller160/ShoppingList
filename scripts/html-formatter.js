81fd1fae43bfd1a081fae560f71e1b8dd5cf4004
Craig Miller <craigmiller160@gmail.com>
2019-12-01 17:48:56 -0500

    Began working on HTML format

--- scripts/html-formatter.js
+++ scripts/html-formatter.js
@@ -1,2 +1,17 @@
-const xmlserializer = require('xmlserializer');
 const { JSDOM } = require('jsdom');
+
+const dom = new JSDOM('<!DOCTYPE html><head /><body /></html>');
+const document = dom.window.document;
+
+const createElement = ({ tagName, textContent }) => {
+	const elem = document.createElement(tagName);
+	elem.textContent = textContent;
+	return elem;
+};
+
+document.head.appendChild(createElement({ 
+	tagName: 'title', 
+	textContent: 'Shopping List'
+}));
+
+console.log(dom.serialize()); // TODO delete this
