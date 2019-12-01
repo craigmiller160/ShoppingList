d74e13e30b4c6dfa846df1c9355f8987545d85d0
Craig Miller <craigmiller160@gmail.com>
2019-12-01 11:07:08 -0500

    Finished remove logic

--- scripts/remove.js
+++ scripts/remove.js
@@ -1,8 +1,29 @@
 const { readData, writeData, openReadline } = require('./io');
 const { formatItemsByIndex } = require('./format');
 
-const removeItem = () => {
+const removeItem = async () => {
 	console.log(formatItemsByIndex());
+	console.log('\n');
+
+	let selection;
+
+	const rl = openReadline();
+	try {
+		selection = await rl.questionSync('ID of Item to Remove: ');
+	}
+	finally {
+		rl.close();
+	}
+
+	const id = parseInt(selection);
+	const data = readData();
+	const index = data.items.findIndex((item) => item.id === id);
+	if (index === -1) {
+		throw new Error(`Cannot find item with ID: ${selection}`);
+	}
+
+	data.items.splice(index, 1);
+	writeData(data);
 };
 
 module.exports = removeItem;
