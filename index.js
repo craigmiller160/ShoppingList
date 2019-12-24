9cc3c12ab8ccc695c332e117a67ed70cac8769de
Craig Miller <craigmiller160@gmail.com>
2019-12-24 10:25:10 -0500

    Made progress working on HTML format

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const addItem = require('./scripts/add');
 const viewData = require('./scripts/view');
 const removeItem = require('./scripts/remove');
 const printItem = require('./scripts/print');
+const formatHtml = require('./scripts/html-formatter'); // TODO remove this
 
 commander
 	.option('-a, --add', 'Add an item to the shopping list')
 	.option('-r, --remove', 'Remove an item to the shopping list')
 	.option('-p, --print', 'Print the shopping list')
 	.option('-v, --view', 'View the shopping list')
+	.option('--html', 'HTML test logic') // TODO remove this
 	.parse(process.argv);
 
 if (commander.add) {
@@ -22,6 +24,10 @@ else if (commander.print) {
 }
 else if (commander.view) {
 	viewData();
+}
+else if (commander.html) {
+	// TODO delete this section
+	formatHtml();
 }
 else {
 	throw new Error('Invalid arguments.');
--- scripts/html-formatter.js
+++ scripts/html-formatter.js
@@ -1,17 +1,72 @@
 const { JSDOM } = require('jsdom');
+const path = require('path'); // TODO remove this
+const formatXml = require('xml-formatter');
+const { readData } = require('./io');
+const fs = require('fs'); // TODO remove this
 
-const dom = new JSDOM('<!DOCTYPE html><head /><body /></html>');
-const document = dom.window.document;
+const DOCTYPE = '<!DOCTYPE html>';
 
-const createElement = ({ tagName, textContent }) => {
-	const elem = document.createElement(tagName);
-	elem.textContent = textContent;
-	return elem;
+const css = `
+	body {
+		font-family: sans-serif;
+	}
+
+	.row {
+		display: flex;
+		flex-direction: row;
+	}
+
+	.col {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.title {
+		text-align: center;
+		border-bottom: 1px solid lightgray;
+		margin: 0 auto;
+		width: 80%;
+	}
+`;
+
+const formatDataHtml = () => {
+	const data = readData();
+
+	const dom = new JSDOM('<html><head /><body /></html>');
+	const document = dom.window.document;
+
+	// TODO figure out a way to move this out of this function
+	const createElement = ({ tagName, textContent, className }) => {
+		const elem = document.createElement(tagName);
+		if (textContent) {
+			elem.textContent = textContent;
+		}
+		if (className) {
+			elem.className = className;
+		}
+		return elem;
+	};
+
+	document.head.appendChild(createElement({ 
+		tagName: 'title', 
+		textContent: 'Shopping List'
+	}));
+	document.head.appendChild(createElement({
+		tagName: 'style',
+		textContent: css
+	}));
+
+	document.body.appendChild(createElement({
+		tagName: 'div',
+		className: 'title'
+	}));
+	document.querySelector('div.title').appendChild(createElement({
+		tagName: 'h1',
+		textContent: 'Shopping List'
+	}));
+
+	const formatted = formatXml(dom.serialize());
+	fs.writeFileSync(path.resolve(require.main.path, 'list.html'), formatted, 'utf8'); // TODO I don't want to write this here
 };
 
-document.head.appendChild(createElement({ 
-	tagName: 'title', 
-	textContent: 'Shopping List'
-}));
-
-console.log(dom.serialize()); // TODO delete this
+module.exports = formatDataHtml;
