8b30dba2ab2582b5a9b67501ada8d7aa7c46fd66
Craig Miller <craigmiller160@gmail.com>
2019-12-26 13:36:35 -0500

    Integrated dev vs prod data into list printing

--- scripts/io.js
+++ scripts/io.js
@@ -6,6 +6,7 @@ const readline = require('readline');
 const DATA_FILE_PATH = path.resolve(require.main.path, './data/list.json');
 const DEV_DATA_FILE_PATH = path.resolve(require.main.path, './data/dev-list.json');
 const OUTPUT_PATH = path.resolve(require.main.path, 'ShoppingList.html');
+const DEV_OUTPUT_PATH = path.resolve(require.main.path, 'ShoppingList_dev.html');
 
 const DEFAULT_DATA = {
 	index: 0,
@@ -13,6 +14,7 @@ const DEFAULT_DATA = {
 };
 
 const dataFilePath = process.env.PROD ? DATA_FILE_PATH : DEV_DATA_FILE_PATH;
+const outputFilePath = process.env.PROD ? OUTPUT_PATH : DEV_OUTPUT_PATH;
 
 const createBackupFilePath = () => {
 	const timestamp = moment().format('YYYYMMDDHHmmss');
@@ -50,8 +52,8 @@ const openReadline = () => {
 };
 
 const writeShoppingList = (html) => {
-	fs.writeFileSync(OUTPUT_PATH, html, 'utf8');
-	console.log(`Shopping List written to: ${OUTPUT_PATH}`);
+	fs.writeFileSync(outputFilePath, html, 'utf8');
+	console.log(`Shopping List written to: ${outputFilePath}`);
 };
 
 module.exports = {
