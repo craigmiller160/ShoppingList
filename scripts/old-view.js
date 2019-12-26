6fefcc32d9f1091fd865f69d17c4feb1036f8ac5
Craig Miller <craigmiller160@gmail.com>
2019-12-26 13:33:07 -0500

    Removed old scripts

--- scripts/old-view.js
+++
@@ -1,8 +0,0 @@
-const { prettyFormatList } = require('./format');
-
-const viewData = () => {
-	const formattedData = prettyFormatList();
-	console.log(formattedData);
-};
-
-module.exports = viewData;
